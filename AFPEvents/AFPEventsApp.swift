d86360194409e2201cdd9261d7e6e7a9bb4b889b
Mickaël MAS <appiwedia@gmail.com>
2021-12-13 09:23:42 +0100

    Initial Commit

---
+++ AFPEvents/AFPEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFPEventsApp.swift
+//  AFPEvents
+//
+//  Created by Mickael Mas on 13/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AFPEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
